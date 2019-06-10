5e7f478ed46334f649f810799ca5039f4de91988
AxlMi <axelmichel60@outlook.fr>
2019-06-11 00:03:43 +0200

    Create DB.py

---
+++ DB.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+# coding: utf-8
+import mysql.connector
+
+
+""" This script will connect to mysql and create : a user, an database ,
+ an table of data to fill whit the API of openfoodfact"""
+
+class Database:
+
+    """ this function will connect the user to the database"""
+    
+    def connect_with_user(self, user_acc, passw, db):
+        try:
+            self.mydb = mysql.connector.connect(
+            host="localhost",
+            user=user_acc,
+            passwd=passw,
+            database=db,)
+            self.mycursor = self.mydb.cursor()
+            print('Connexion à {}'.format(user_acc))
+        except mysql.connector.errors.ProgrammingError: 
+            print("votre nom d'utilisateur ou mot de passe est incorrect")
+
+    """ to create a user with creation and modification rights"""
+
+    def create_user(self,):
+        try:
+            user = "CREATE USER 'StudentOF'@'localhost' IDENTIFIED BY '1Ksable$';"
+            privilege = "GRANT ALL PRIVILEGES ON * . * to 'StudentOF'@'localhost';"
+            flush = "FLUSH PRIVILEGES;"
+            self.mycursor.execute(user)
+            self.mycursor.execute(privilege)
+            self.mycursor.execute(flush)
+            print("Création de l'utilisateur")
+        except mysql.connector.errors.DatabaseError: #if the user already exists we validate anyway
+            print("creation de l'utilisateur")
+    
+    
+    def create_db(self,):
+        self.mycursor = self.mydb.cursor()
+        data_name = 'CREATE DATABASE IF NOT EXISTS openfoodfact;'
+        self.mycursor.execute(data_name)
+        print("Création de la base de données")
+
+
+    def create_table(self,):
+        table_product = """CREATE TABLE IF NOT EXISTS product (
+            id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+            Product_name VARCHAR(30) NOT NULL,
+            Categories VARCHAR(30) NOT NULL,
+            Nutrition_grade CHAR(1),
+            Brands VARCHAR(30),
+            Stores VARCHAR(30),
+            url TEXT(80),
+            PRIMARY KEY (id)
+            )
+            ENGINE=INNODB;"""
+        self.mycursor.execute(table_product)
+        print('la table Product a été crée')
+
+def main():
+    DB = Database()
+    DB.connect_with_user(input('entrez votre nom d\'utilisateur mysql : '),
+    input('entrez votre MDP : '), '')
+    DB.create_user()
+    DB.connect_with_user('StudentOF', '1Ksable$', 'openfoodfact')
+    DB.create_db()
+    DB.create_table()
+    
+main()
--- Openfood.py
+++ Openfood.py
@@ -1,25 +1,62 @@
+#! /usr/bin/env python
+# coding: utf-8
+
 import requests
 import json
+import mysql.connector
 
-final_products = []
+class import_db:
+
+    def connect(self,):
+        self.mydb = mysql.connector.connect(
+            host="localhost",
+            user='StudentOF',
+            passwd='1Ksable$',
+            database="openfoodfact")
+        self.mycursor = self.mydb.cursor()
+        
+
+    for i in range(1, 2):
+        products = requests.get("https://world.openfoodfacts.org/cgi/search.pl",{
+                    'action': 'process',
+                    'tagtype_0': 'categories', #categories selected
+                    'tag_contains_0': 'contains', #contains or not
+                    'sort_by': 'unique_scans_n',
+                    'countries': 'France',
+                    'json': 1,
+                    'page': 553444000,
+                    'page_size' : 100
+                    })
+    
+        #def add_values(key, type_key, values, values_key)
+         #   self.connect()
+      #mycursor.execute("CREATE TABLE product (name VARCHAR(255), address VARCHAR(255))")
+
+        
+        response = json.loads(products.text)
+        
+        for product in response['products']:
+            print(product['product_name'])
+            #for key, values in product.items():
+               # print('{} {}'.format(key, values))
+
+        def create_table(self,):
+            self.connect()
+            mycursor.execute("CREATE TABLE product (name VARCHAR(255), address VARCHAR(255))")
 
 
-for i in range(1, 150):
-    products = requests.get("https://world.openfoodfacts.org/cgi/search.pl",{
-                'action': 'process',
-                'tagtype_0': 'categories', #categories selected
-                'tag_contains_0': 'contains', #contains or not
-                'sort_by': 'unique_scans_n',
-                'countries': 'France',
-                'json': 1,
-                'page': 553444000,
-                'page_size' : 100
-                })
-    print(products.text)
-    response = json.loads(products.text)
+    #with open('dataok.json', 'w') as f:
+     #   f.write(y)
 
-    for product in response['products']:
+    #print(x)
+    #with open('datatest.json', 'w')as f:
+     #   f.write(json.dumps(products.text, indent=50))
+   # response = json.loads(products.text)
+    #print(response)
+    #print(response)
+
+    #for product in response['products']:
         # ajouter produits voulu sur sql
-        final_products.append({
-            'code': product['code']
-        })
+     #   final_products.append({
+      #      'code': product['code']
+       # })
