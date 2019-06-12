13020bee8c27793dc905ba765807cfd6e8e35fc4
AxlMi <axelmichel60@outlook.fr>
2019-06-12 02:54:39 +0200

    Add grade_score

--- Openfood.py
+++ Openfood.py
@@ -5,7 +5,7 @@ import requests
 import json
 import mysql.connector        
 
-for i in range(1, 1000):
+for i in range(1, 500):
     mydb = mysql.connector.connect(
         host="localhost",
         user='StudentOF',
@@ -23,13 +23,17 @@ for i in range(1, 1000):
                 'page': 553444000,
                 'page_size' : 100
                 })
-    
+
     response = json.loads(products.text)
-    
+    #with open("test1page.json", "w") as f:
+     #   f.write(json.dumps(response, indent=4))
+
+
+#    
     for product in response['products']:
-        #print(product['nutrition_grades'])
+#        #print(product['nutrition_grade_fr'])
         sql = "INSERT INTO product (Product_name, Categories, Nutrition_grade, Brands, Stores, url_product) VALUES (%s, %s, %s, %s, %s, %s)"
-        val = (product['product_name'], product['categories'], "a", product['brands'], product['stores'], product['url'])
+        val = (product['product_name'], product['categories'], product.get('nutrition_grade_fr'), product['brands'], product['stores'], product['url'])
         mycursor.execute(sql, val)
         mydb.commit()
 
